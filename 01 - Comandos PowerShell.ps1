8aa6058f45dcb82423d961c4ad719f0bd12b2470
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-03-04 09:14:02 -0300

    Add: az version

--- 01 - Comandos PowerShell.ps1
+++ 01 - Comandos PowerShell.ps1
@@ -1,1 +1,14 @@
+#Pegar a data do sistema
 Get-date
+
+#Pegar o versão do Azure
+az version
+{  "azure-cli": "2.57.0",         
+  "azure-cli-core": "2.57.0",
+  "azure-cli-telemetry": "1.1.0",
+  "extensions": {
+    "ai-examples": "0.2.5",
+    "ml": "2.22.0",
+    "ssh": "2.0.2"
+  }
+}
